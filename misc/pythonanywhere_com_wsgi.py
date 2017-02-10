d42b75ecf637d980c6e7e0fa50b242e41337248e
Ming Ming <mint2648@gmail.com>
2017-02-11 00:52:10 +0800

    Add sample PAW WSGI config file

---
+++ misc/pythonanywhere_com_wsgi.py
@@ -0,0 +1,19 @@
+# This file contains the WSGI configuration required to serve up your
+# web application at http://<your-username>.pythonanywhere.com/
+# It works by setting the variable 'application' to a WSGI handler of some
+# description.
+#
+# The below has been auto-generated for your Flask project
+
+import sys
+
+# add your project directory to the sys.path
+project_home = u'/home/<your-username>/google-search-telegram-bot/src'
+if project_home not in sys.path:
+    sys.path = [project_home] + sys.path
+
+from app.paw_app import PawApp
+PawApp().run()
+
+# import flask app but need to call it "application" for WSGI to work
+from app.paw_app import flask_app as application
